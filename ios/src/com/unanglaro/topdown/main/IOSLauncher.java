f852cfa59a62e7c28c0b13d312dcffdcc5ae3db3
Tategami99 <k07bautista@gmail.com>
2022-12-11 17:45:50 -0800

    initial - revision

---
+++ ios/src/com/unanglaro/topdown/main/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.unanglaro.topdown.main;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.unanglaro.topdown.main.rpgGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new rpgGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
